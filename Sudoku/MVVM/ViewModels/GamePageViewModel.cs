850c30f5540d810f78716c7eea89ff1f25fbaad2
AndyJL1999 <andylainez115@hotmail.com>
2023-08-17 18:09:11 -0400

    --Added timer to GamePage

--- Sudoku/MVVM/ViewModels/GamePageViewModel.cs
+++ Sudoku/MVVM/ViewModels/GamePageViewModel.cs
@@ -2,33 +2,43 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Sudoku.MVVM.Models;
-using Sudoku.MVVM.Views;
 using System.Collections.ObjectModel;
+using Timer = System.Timers.Timer;
 
 namespace Sudoku.MVVM.ViewModels
 {
     public partial class GamePageViewModel : ObservableObject
     {
         [ObservableProperty]
-        private ObservableCollection<SudokuCell> _sudokuPattern;
+        private ObservableCollection<SudokuCell> _sudokuPattern = new ObservableCollection<SudokuCell>();
         [ObservableProperty]
         private SudokuCell _chosenCell;
         [ObservableProperty]
         private string _numberChoice;
         [ObservableProperty]
         private string _errorCounter;
+        [ObservableProperty]
+        private string _clock;
 
         private Random _rnd;
         private int _errorNum;
+        private int _clockNum;
+        private Timer _timer;
 
         public GamePageViewModel()
         {
             _rnd = new Random();
+            _timer = new Timer(1000);
+            _clockNum = 0;
 
             ErrorCounter = "Mistakes: 0/3";
             NumberChoice = "123456789";
+            Clock = "Time: 0:00";
 
             SetBoard();
+
+            _timer.Elapsed += (sender, e) => HandleTimer();
+            _timer.Start();
         }
 
         [RelayCommand]
@@ -88,14 +98,13 @@ namespace Sudoku.MVVM.ViewModels
         {
             _errorNum = 0;
 
-            SudokuPattern = new ObservableCollection<SudokuCell>();
             string viablePattern = "123456789";
 
             for (int i = 0; i < 9; i++)
             {
-                viablePattern = new (viablePattern.OrderBy(x => _rnd.Next()).ToArray());
+                viablePattern = new(viablePattern.OrderBy(x => _rnd.Next()).ToArray());
 
-                for(int j = 0; j < 9; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     SudokuPattern.Add(new SudokuCell
                     {
@@ -105,6 +114,14 @@ namespace Sudoku.MVVM.ViewModels
                     });
                 }
             }
+        }
+
+
+        void HandleTimer()
+        {
+            _clockNum++;
+            
+            Clock = $"Time: " + TimeSpan.FromSeconds(_clockNum).ToString("mm':'ss");
         }
 
     }
